d9761dcd539ced019ee02ae96158a04e7e631081
Majid <mj_azani@hotmail.com>
2015-08-03 15:54:43 +0430

    code improvement

--- brew.sh
+++ brew.sh
@@ -14,13 +14,13 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # Check for Homebrew
 if test ! $(which brew)
 then
-	echo "  Installing Homebrew for you."
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
-else 
-	# Make sure we’re using the latest Homebrew.
-	brew update
-	# Upgrade any already-installed formulae.
-	brew upgrade
+    echo "  Installing Homebrew for you."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /tmp/homebrew-install.log
+else
+    # Make sure we’re using the latest Homebrew.
+    brew update
+    # Upgrade any already-installed formulae.
+    brew upgrade
 fi
 
 # Install homebrew packages
