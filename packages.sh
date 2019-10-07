0c2e96f9c9db1a01acf88c87a944fd603bbc58be
Majid <mj_azani@hotmail.com>
2019-10-07 12:51:49 +0330

    removing redundant packages

--- packages.sh
+++ packages.sh
@@ -14,28 +14,6 @@ npm install -g preprocess
 npm install -g csscomb
 
 ###############################################################################
-# Sublime Text                                                                #
-###############################################################################
-
-echo "Setting Sublime preferences"
-
-# Install Sublime Text settings
-cp -r sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Preferences.sublime-settings 2> /dev/null
-
-# Install Sublime Color Scheme
-cp -r "sublime/base16-eighties.dark (SL).tmTheme" "~/Library/Application Support/Sublime Text*/Packages/User/base16-eighties.dark (SL).tmTheme" 2> /dev/null
-
-# Install Sublime Key Bindings
-cp -r "sublime/Default (OSX).sublime-keymap" "~/Library/Application Support/Sublime Text*/Packages/User/Default (OSX).sublime-keymap" 2> /dev/null
-
-# Install Sublime Packages
-cp -r sublime/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Package\ Control.sublime-settings 2> /dev/null
-
-# Install Sublime Snippets
-cp -r sublime/subl-snippets ~/Library/Application\ Support/Sublime\ Text*/Packages/User 2> /dev/null
-
-
-###############################################################################
 # Extra packages                                                              #
 ###############################################################################
 
@@ -44,8 +22,7 @@ npm install -g grunt-cli
 npm install -g typescript
 npm install -g gulp-cli
 npm install express
-npm install preprocess
-npm install bower
+npm install -g bower
 npm install mongoose
 
 # Gem packages
