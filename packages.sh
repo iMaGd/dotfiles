0212e99e8c103fed3ac6c3c90a05451a06336bd0
Majid <mj_azani@hotmail.com>
2019-10-07 13:03:31 +0330

    some cleanup

--- packages.sh
+++ packages.sh
@@ -7,23 +7,15 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # use none-ssl node respository
 npm config set registry http://registry.npmjs.org/
-# packages for sublimelinter
+
+# NPM packages
+npm install -g grunt-cli
+npm install -g gulp-cli
+npm install -g bower
+npm install -g typescript
+npm install -g browserify
 npm install -g csslint
 npm install -g jshint
-npm install -g preprocess
-npm install -g csscomb
-
-###############################################################################
-# Extra packages                                                              #
-###############################################################################
-
-# Node packages
-npm install -g grunt-cli
-npm install -g typescript
-npm install -g gulp-cli
-npm install express
-npm install -g bower
-npm install mongoose
 
 # Gem packages
 gem install compass
