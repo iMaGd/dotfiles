d7ddc39112a6de05ad5c82597b8c2f45aa70327d
Majid <mj_azani@hotmail.com>
2019-10-07 12:53:19 +0330

    backup sublime perferences

---
+++ update.sh
@@ -0,0 +1,13 @@
+
+# Sync the repo by latest changes for vscode in local mashine
+rsync -av --progress --delete-after ~/Library/Application\ Support/Code/User ./vscode --exclude workspaceStorage --exclude globalStorage --exclude syncLocalSettings.json
+
+# Generate extensions list
+code --list-extensions | xargs -L 1 echo code --install-extension >> vscode/extensions.sh
+
+# Copy sublime setting to repo
+rsync -av ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings ./sublime/
+rsync -av ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/base16-eighties.dark.tmTheme ./sublime/
+rsync -av ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings ./sublime/
+rsync -av ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap ./sublime/
+rsync -av ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/subl-snippets ./sublime/
