687ef01faeb934af1dfa0388b65963daa7623d6d
Majid <mj_azani@hotmail.com>
2025-08-31 22:45:11 +0930

    make project directories

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 
 # Set vscode setting
-rsync -av ./vscode/ ~/Library/Application\ Support/Code/
+# rsync -av ./vscode/ ~/Library/Application\ Support/Code/
 
 # Set sublime setting
 rsync -av ./sublime/ ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-
 
 # iTerm2 - Specify the preferences directory
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
 
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+# Create projects directory
+mkdir -p ~/projects/{herds,ddevs,pstorm,crafts/plugins,wp-development/plugins}
